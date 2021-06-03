2b974cddd96b8c7ce8d2df6114085f59a2a5590d
brenomoreirap <85278831+brenomoreirap@users.noreply.github.com>
2021-06-03 06:01:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = window.document.getElementById('txtano')
+    var res = window.document.getElementById('res')
+    var img = window.document.getElementById('imagem')
+
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('Ocorreu um erro! Verifique os dados e tente novamente.')
+    } else {
+        var fsex = window.document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var gen = ''
+        var tipo = ''
+
+        if (fsex[0].checked) {
+            gen = 'homem'
+            if (fano >= 0 && fano < 10) {
+                img.src = './imagens/crianca-m.png'
+                tipo = 'criança'
+            } else if (fano < 21) {
+                img.src = './imagens/jovem-m.png'
+                tipo = 'jovem'
+            } else if (fano < 50) {
+                img.src = './imagens/adulto-m.png'
+                tipo = 'adulto'
+            } else {
+                img.src = './imagens/idoso-m.png'
+                tipo = 'idoso'
+            }
+        } else if (fsex[1].checked) {
+            gen = 'mulher'
+            if (fano >= 0 && fano < 10) {
+                img.src = './imagens/crianca-f.png'
+                tipo = 'criança'
+            } else if (fano < 21) {
+                img.src = './imagens/jovem-f.png'
+                tipo = 'jovem'
+            } else if (fano < 50) {
+                img.src = './imagens/adulto-f.png'
+                tipo = 'adulta'
+            } else {
+                img.src = './imagens/idoso-f.png'
+                tipo = 'idosa'
+            }
+        }
+        res.innerHTML = `Detectamos ${gen} ${tipo} com ${idade} anos de idade`
+    }
+
+}
